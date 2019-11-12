c7907e6aa7962a908cbbd56c5ca755ed33816a5b
zhaoxihe <739613839@qq.com>
2019-11-12 09:28:14 +0800

    week10

--- week10/code/my.h
+++ week10/code/my.h
@@ -2,6 +2,3 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <wait.h>
-#include <fcntl.h>
-#include <string.h>
---
+++ week9/code/test/fork1.c
@@ -0,0 +1,9 @@
+#include "my.h"
+int k = 0;
+int main()
+{
+	static int r=9;
+	pid_t pid;
+	int y=9;
+	)
+}
