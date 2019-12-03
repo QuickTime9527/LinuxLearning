cdcacc7f8c79003c50c7674f215e422b45730328
zhaoxihe <739613839@qq.com>
2019-12-03 08:59:32 +0800

    week13

--- FileSystem/code/my.c
+++ FileSystem/code/my.c
@@ -2,7 +2,6 @@
 
 void showMenu()
 {
-	printf("this is menu\n");
 	printf("*********************************\n");
 	printf("1.创建新文件\n");
 	printf("2.写文件\n");
@@ -10,17 +9,91 @@ void showMenu()
 	printf("4.修改文件权限\n");
 	printf("5.查看当前文件的权限\n");
 	printf("6.修改文件权限\n");
+	printf("7.退出系统\n");
 	printf("*********************************\n");
 }
 void createFile()
 {
-	FILE *fpWrite = fopen("data.txt","w");
-	
+
+    //保存运行次数的文件
+    FILE *fileP;
+    char *fileName;      //保存在工程目录下
+	printf("请输入文件名：");
+	scanf("%s",fileName);
+
+    //使用“读入”方式打开文件
+    fileP = fopen(fileName, "r");
+
+    //如果文件不存在
+    if (fileP == NULL)
+    {
+        //使用“写入”方式创建文件
+        fileP = fopen(fileName, "w");
+	   if(fileP!=NULL)
+		printf("创建文件成功！\n");
+    }else
+     {
+	printf("创建失败！ 文件已存在\n");
+     }
+    
+    //关闭文件
+    fclose(fileP);
+}
+
+void writeFile()
+{
+	FILE *fileP;
+	int ch;
+      char *fileName;      //保存在工程目录下
+	printf("请输入文件名：");
+	scanf("%s",fileName);
+	fileP = fopen(fileName,"r"); //以read
+	if(fileP == NULL)
+	{
+		printf("该文件不存在！\n");
+		while((ch = getchar()) != '\n' && ch != EOF);
+	}else{
+		fileP = fopen(fileName,"w");
+		fprintf(fileP,"yes you are right ！");
+	}
 }
 
 void run()
 {
+	int choice;
+	int signal = 0;
 	showMenu();
+	while(signal!=1)
+ 	{
+
+		printf("请输入你的选择：");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+			  createFile();
+			  break;
+			case 2:
+			  writeFile();
+			  break;
+			case 3:
+			  break;
+			case 4:
+			  break;
+			case 5:
+			  break;
+			case 6:
+			  break;
+			case 7:
+			  signal = 1;
+			  break;
+			default:
+			  printf("你是不是龙鸣啊你？\n");
+			  break;
+		}
+	}	
+	printf("Bye!\n");
+
 }
 
 
--- FileSystem/code/my.h
+++ FileSystem/code/my.h
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+
+#define CLEAR_BUF \
+    {\
+    int ch; \
+while ((ch = getchar()) != EOF && ch != '\n')\
+        {\
+        ; \
+        }\
+    }
+
+
+void run();  //程序运行
 void showMenu();    //显示菜单
 void createFile();  //创建文件
-void run();  //程序运行
+
+void writeFile(); //写文件
